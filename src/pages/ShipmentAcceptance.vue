17c9f1821300e4adbd0ecc7f891c3aafd696b21a
Maksim Molovcev <molir10@mail.ru>
2019-03-12 21:07:20 +0300

    shipment-acceptance

--- src/pages/ShipmentAcceptance.vue
+++ src/pages/ShipmentAcceptance.vue
@@ -7,26 +7,37 @@
             @input="onInput"
             @decode="onDecode"
         />
-        <button
-            ref="button-next"
-            class="w-100 btn btn-success"
-            :disabled="!VIN"
-            @click="acceptShipment"
-        >
-            Подтвердить
-        </button>
         <b-modal
-            v-model="isModalShown"
+            v-if="car"
+            v-model="isScanModalShown"
             class="text-center"
             header-border-variant="success"
             title="Готово!"
             ok-variant="success"
-            ok-only
+            ok-title="Подтвердить"
+            cancel-title="Отмена"
+            cancel-variant="danger"
             centered
+            @ok="acceptShipment"
             @hidden="hideScanScreen"
         >
             <h4 class="font-weight-normal">VIN-номер</h4>
             <h5>{{ VIN }}</h5>
+            <h6>{{ car.model }}<br>{{ car.color }}</h6>
+        </b-modal>
+        <b-modal
+            v-if="car"
+            v-model="isSuccessModalShown"
+            class="text-center"
+            header-border-variant="success"
+            title="Готово!"
+            ok-variant="success"
+            ok-title="Ок"
+            ok-only
+            centered
+            @hidden="hideScanScreen"
+        >
+            Приемка автомобиля {{ car.VIN }} успешно подтверждена!
         </b-modal>
     </div>
 </template>
@@ -41,22 +52,33 @@ export default {
         Scanner,
     },
     data: () => ({
-        isModalShown: false,
+        isScanModalShown: false,
+        isSuccessModalShown: false,
         VIN: '',
+        car: null,
     }),
     computed: {
         ...mapState(['isScanScreenShown']),
     },
     methods: {
-        async acceptShipment() {},
+        async acceptShipment() {
+            const params = {
+                ...this.car,
+                status: 'compound in',
+            };
+            await this.$http.put(`cars/${this.VIN}`);
+            this.isSuccessModalShown = true;
+        },
         onInput(result) {
             this.VIN = result;
         },
-        onDecode(result) {
+        async onDecode(result) {
             this.VIN = result;
-            this.isModalShown = true;
+            this.car = await this.$http.get(`cars/${this.VIN}`);
+            this.isScanModalShown = true;
         },
         hideScanScreen() {
+            this.VIN = '';
             this.$store.commit('hideScanScreen');
         },
     },
